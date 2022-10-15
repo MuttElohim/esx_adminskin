212d3564c1233ec81b9d4ee109b87febbad7f8f3
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2022-10-15 05:42:11 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+  ['title_adminskin'] = 'Admin Skin',
+  ['title_custom'] = 'Custom',
+  ['title_custom2'] = 'Personal Custom',
+  ['title_deletecustom'] = 'Delete Personal',
+  ['title_dress'] = 'Dress',
+  ['command_adminskin'] = 'Toggle Admin Skin',
+  ['command_adminskinmenu'] = 'Open Admin Skin Menu',
+}
