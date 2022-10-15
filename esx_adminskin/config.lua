2f09806252a796434c22aff887c4751936abd70f
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2022-10-15 05:41:49 +0200

    Delete esx_adminskin directory

--- esx_adminskin/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.Locale = GetConvar('esx:locale', 'en')
-
-Config.Custom = true
-
-Config.CustomSkin = true
--- esx_adminskin/locales/en.lua
+++
@@ -1,9 +0,0 @@
-Locales['en'] = {
-  ['title_adminskin'] = 'Admin Skin',
-  ['title_custom'] = 'Custom',
-  ['title_custom2'] = 'Personal Custom',
-  ['title_deletecustom'] = 'Delete Personal',
-  ['title_dress'] = 'Dress',
-  ['command_adminskin'] = 'Toggle Admin Skin',
-  ['command_adminskinmenu'] = 'Open Admin Skin Menu',
-}
